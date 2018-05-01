b68df5385add72eba32b6fb323c29df25be5cc82
Marcus Crane <marcus.crane@xero.com>
2018-05-02 10:41:50 +1200

    Added some more tests and parsing bits

--- parsers/requirements.go
+++ parsers/requirements.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-// LoadRequirements takes a file and loads it returning string data
+// LoadFile takes a file and loads it returning string data
 func LoadFile(path string) string {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -17,6 +17,24 @@ func LoadFile(path string) string {
 // ParseRequirements takes a requirements.txt string and parses it into a string array
 func ParseRequirements(requirements string) []string {
 	return strings.Split(requirements, "\n")
+}
+
+// PruneVersion parses a requirements entry and removes any version pin if it exists
+func PruneVersion(requirement string) string {
+	if strings.Contains(requirement, "==") {
+		return strings.Split(requirement, "==")[0]
+	}
+	return requirement
+}
+
+// PruneRequirements parses a slice of requirements names and removes the versioning if it exists
+func PruneRequirements(requirementsString string) []string {
+	requirements := ParseRequirements(requirementsString)
+	var reqs []string
+	for _, requirement := range requirements {
+		reqs = append(reqs, PruneVersion(requirement))
+	}
+	return reqs
 }
 
 // LoadRequirements loads the provided path and then parses the file it finds
--- parsers/requirements_test.go
+++ parsers/requirements_test.go
@@ -23,6 +23,36 @@ func TestParseRequirements(t *testing.T) {
 	assert.Equal(list, expected)
 }
 
+func TestPruneVersion(t *testing.T) {
+	assert := assert.New(t)
+
+	req := "amqp==2.2.2"
+	actual := PruneVersion(req)
+
+	expected := "amqp"
+	assert.Equal(actual, expected)
+}
+
+func TestPruneVersionMissingVersion(t *testing.T) {
+	assert := assert.New(t)
+
+	req := "amqp"
+	actual := PruneVersion(req)
+
+	expected := "amqp"
+	assert.Equal(actual, expected)
+}
+
+func TestPruneVersions(t *testing.T) {
+	assert := assert.New(t)
+
+	reqs := "amqp==2.2.2\nbeautifulsoup4==4.6.0\nbilliard==3.5.0.3"
+	actual := PruneRequirements(reqs)
+
+	expected := []string{"amqp", "beautifulsoup4", "billiard"}
+	assert.Equal(expected, actual)
+}
+
 func TestLoadRequirements(t *testing.T) {
 	assert := assert.New(t)
 
